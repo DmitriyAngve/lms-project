8c2f23913711b2f7a31621c4bdd2a8f8101a7d84
DmitriyAngve <kirirch129@gmail.com>
2023-09-20 14:16:20 +0700

    #

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,4 @@
-import { Sidebar } from "../_components/sidebar";
+import { Sidebar } from "./_components/sidebar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (

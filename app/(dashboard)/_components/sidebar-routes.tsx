9350ab4fce48dfd7f51d977c15f2d2103c9b10c6
DmitriyAngve <kirirch129@gmail.com>
2023-09-21 10:32:20 +0700

    #

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Layout, Compass } from "lucide-react";
+import { Layout, Compass, List, BarChart } from "lucide-react";
 import { SidebarItem } from "./sidebar-item";
 
 const guestRoutes = [
@@ -13,6 +13,19 @@ const guestRoutes = [
     icon: Compass,
     label: "Browse",
     href: "/search",
+  },
+];
+
+const teacherRoutes = [
+  {
+    icon: List, // иконка, отображающаяся рядом с маршрутом
+    label: "Courses", // текстовая метка, отображающаяся рядом с иконкой
+    href: "/teacher/courses", // это сво-во указывает на URL-адрес, на который будет осуществляться переход при выборе маршрута
+  },
+  {
+    icon: BarChart,
+    label: "Analytics",
+    href: "/teacher/analytics",
   },
 ];
 
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,15 +1,34 @@
 "use client";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
 
 export const NavbarRouters = () => {
   const pathname = usePathname();
-  const router = useRouter();
+
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter");
 
   return (
     <div className="flex gap-x-2 ml-auto">
-      <UserButton />
+      {isTeacherPage || isPlayerPage ? (
+        <Link href="/">
+          <Button size="sm" variant="ghost">
+            <LogOut className="h-4 w-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button size="sm" variant="ghost">
+            Teacher mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };

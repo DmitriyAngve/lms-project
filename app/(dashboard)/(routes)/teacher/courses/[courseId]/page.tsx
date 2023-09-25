09206d434cf134d9580ec984504dc986372b2469
DmitriyAngve <kirirch129@gmail.com>
2023-09-25 23:10:26 +0700

    #

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -30,9 +30,27 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const totalFields = requiredFields.length; // число всех полей
   const completedFields = requiredFields.filter(Boolean).length; // число полей, которые не равны false
 
-  const completionText = `(${completedFields} / ${totalFields})`;
+  const completionText = `(${completedFields}/${totalFields})`;
 
-  return <div>Course ID Page! {params.courseId}</div>;
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Course setup</h1>
+          <span className="text-sm text-slate-700">
+            Complete all fields {completionText}
+          </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <h2 className="text-xl">Customize your course</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CourseIdPage;

103688363ad00da840657337ac7153a6108e7531
DmitriyAngve <kirirch129@gmail.com>
2023-09-26 22:03:38 +0700

    #

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -4,6 +4,9 @@ import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
+
+import { Pencil } from "lucide-react";
 
 import {
   Form,
@@ -29,6 +32,10 @@ const formSchema = z.object({
 });
 
 const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEdit = () => setIsEditing((current) => !current); // это функция обновления состояния setIsEditing (когда вызывается эта функция, это приводит к изменению isEditing с true на false и наоборот)
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData,
@@ -40,7 +47,49 @@ const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
     console.log(values);
   };
 
-  return <div className="mt-6 border bg-slate-100 round p-4">Title Form</div>;
+  return (
+    <div className="mt-6 border bg-slate-100 rounded p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course title
+        <Button onClick={toggleEdit} variant="ghost">
+          {/* указываю Cancel во фрагменте потому что это условный рендеринг. Если isEditing = false, то Cancel не рендерится! */}
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit title
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && <p className="text-sm mt-2">{initialData.title}</p>}
+      {isEditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. Advanced web development"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      )}
+    </div>
+  );
 };
 
 export default TitleForm;

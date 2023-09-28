3c7eab0e6aef1b27cdf9f2a5a51d6a1ce6b08641
DmitriyAngve <kirirch129@gmail.com>
2023-09-28 23:07:26 +0700

    #

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
@@ -11,6 +11,8 @@ import {
 } from "@hello-pangea/dnd";
 
 import { cn } from "@/lib/utils";
+import { Grip, Pencil } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 
 interface ChaptersListProps {
   items: Chapter[];
@@ -40,11 +42,15 @@ export const ChaptersList = ({
   }
 
   return (
+    // "DragDropContext" - обёртка. "onDragEnd={() => {}}" - ф-ия, которая будет завершаться после перетаскивания элементов
     <DragDropContext onDragEnd={() => {}}>
+      {/* Droppable - компонент, определяющий область, в которой можно перетаскивать элементы. "droppableId" - уникальный идентификатор области */}
       <Droppable droppableId="chapters">
+        {/* далее ф-ия рендер. Отвечает за отображание самих элементов списка. Стили зависят от "chapter.isPublished" */}
         {(provided) => (
           <div {...provided.droppableProps} ref={provided.innerRef}>
             {chapters.map((chapter, index) => (
+              // "Draggable" - компонент, представляющий элемент списка, который можно перетаскивать. "index" - позиция элемента в списке.
               <Draggable
                 key={chapter.id}
                 draggableId={chapter.id}
@@ -57,12 +63,40 @@ export const ChaptersList = ({
                       chapter.isPublished &&
                         "bg-sky-100 border-sky-200 text-sky-700"
                     )}
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                  ></div>
+                    ref={provided.innerRef} // этот пропс для отслеживания перетаскиваемых элементов и управлять перемещением
+                    {...provided.draggableProps} // Этот атрибут распаковывает все пропсы, предоставленные компонентом "<Draggable>", которые нужны для перемещения эл-та.
+                  >
+                    <div
+                      className={cn(
+                        "px-2 py-3 border-r border-r-slate-200 hover:bg-slate-300 rounded-l-md transition",
+                        chapter.isPublished &&
+                          "border-r-sky-200 hover:bg-sky-200"
+                      )}
+                      {...provided.dragHandleProps}
+                    >
+                      <Grip className="h-5 w-5" />
+                    </div>
+                    {chapter.title}
+                    <div className="ml-auto pr-2 flex items-center gap-x-2">
+                      {chapter.isFree && <Badge>Free</Badge>}
+                      <Badge
+                        className={cn(
+                          "bg-slate-500",
+                          chapter.isPublished && "bg-sky-700"
+                        )}
+                      >
+                        {chapter.isPublished ? "Published" : "Draft"}
+                      </Badge>
+                      <Pencil
+                        onClick={() => onEdit(chapter.id)}
+                        className="w-4 h-4 cursor-pointer hover:opacity-75 transition"
+                      />
+                    </div>
+                  </div>
                 )}
               </Draggable>
             ))}
+            {provided.placeholder}
           </div>
         )}
       </Droppable>

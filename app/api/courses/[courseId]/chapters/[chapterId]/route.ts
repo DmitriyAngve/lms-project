8b1d4de44af05185462569946a1125c08559e6a9
DmitriyAngve <kirirch129@gmail.com>
2023-09-30 21:33:25 +0700

    #

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,6 +1,12 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+import Mux from "@mux/mux-node";
+
+const { Video } = new Mux(
+  process.env.MUX_TOKEN_ID!,
+  process.env.MUX_TOKEN_SECRET!
+);
 
 export async function PATCH(
   req: Request,
@@ -35,7 +41,31 @@ export async function PATCH(
       },
     });
 
-    // TODO: Handle Video Upload
+    // Проверяем, есть ли значение "videoUrl" в объекте "values". Если оно существует, то это означает, что пользователь пытается загрузить новое видео
+    if (values.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId,
+        },
+      });
+
+      // Если запись muxData найдена, то код выполняется:
+      if (existingMuxData) {
+        await Video.Assets.del(existingMuxData.assetId); // Это означает, что предыдущий видео-файл будет удален с Mux
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          },
+        }); // В том числе удаляется из БД.
+      }
+
+      // После удаления создается новый видео-файл. Он использует "values.videoUrl" в качестве входных данных и настраивает параметры файла (актива), так как политика воспроизведения "public"
+      const asset = await Video.Assets.create({
+        input: values.videoUrl,
+        playback_policy: "public",
+        test: false,
+      });
+    }
 
     return NextResponse.json(chapter);
   } catch (error) {

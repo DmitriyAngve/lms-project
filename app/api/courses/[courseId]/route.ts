5fba8e59deb8187d7234e4aaa4a455c276aa7e73
DmitriyAngve <kirirch129@gmail.com>
2023-10-04 20:42:18 +0700

    #

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -59,12 +59,12 @@ export async function DELETE(
 
 export async function PATCH(
   req: Request,
-  { params }: { params: { courseId: string } }
+  { params }: { params: { courseId: string } } // courseId - это название папки ([courseId])
 ) {
   try {
     const { userId } = auth();
-    const { courseId } = params;
-    const values = await req.json();
+    const { courseId } = params; // извлекаем courseId из параметров запроса
+    const values = await req.json(); // получаем данные из тела HTTP-запроса и разбираем JSON-данные
 
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
@@ -80,7 +80,7 @@ export async function PATCH(
       },
     });
 
-    return NextResponse.json(course);
+    return NextResponse.json(course); // возвращается HTTP-ответ с данными обновленного курса в формате JSOn
   } catch (error) {
     console.log("[COURSE_ID]", error);
     return new NextResponse("Internal Error", { status: 500 });

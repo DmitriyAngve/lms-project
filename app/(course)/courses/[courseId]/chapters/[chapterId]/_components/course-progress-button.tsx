f71ba3fee243a090bfab787f02ab2cc6e9ae0753
DmitriyAngve <kirirch129@gmail.com>
2023-10-07 16:14:29 +0700

    #

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-progress-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-progress-button.tsx
@@ -1,8 +1,14 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useConfettiStore } from "@/hooks/use-confetti-store";
+import axios from "axios";
+import { on } from "events";
 
 import { CheckCircle, XCircle } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface CourseProgressButtonProps {
   chapterId: string;
@@ -17,12 +23,50 @@ export const CourseProgressButton = ({
   isCompleted,
   nextChapterId,
 }: CourseProgressButtonProps) => {
+  const router = useRouter();
+  const confetti = useConfettiStore();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      await axios.put(
+        `/api/course/${courseId}/chapters/${chapterId}/progress`,
+        {
+          isCompleted: !isCompleted, // Это переключение с true на false с помощью "!", чтобы не менять статус завершения главы (Completed) => иными словами, если isCompleted было true, то после выполнения этого кода оно станет false, и наоборот!
+        }
+      );
+
+      // "(!isCompleted && !nextChapterId)" это означает, что пользователь достигнул конца курса (когда пользователь кликнет, то все главы отметятся законченными, потому что это последняя)
+      if (!isCompleted && !nextChapterId) {
+        confetti.onOpen();
+      }
+
+      if (!isCompleted && nextChapterId) {
+        router.push(`/courses/${courseId}/chapters/${nextChapterId}`);
+      }
+
+      toast.success("Progress updated");
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   const Icon = isCompleted ? XCircle : CheckCircle;
 
   return (
-    <Button>
+    <Button
+      onClick={onClick}
+      type="button"
+      variant={isCompleted ? "outline" : "success"}
+      className="w-full md:w-auto"
+    >
       {isCompleted ? "Not Completed" : "Mark as complete"}
-      <Icon className="h-4 w-4" />
+      <Icon className="h-4 w-4 ml-2" />
     </Button>
   );
 };
